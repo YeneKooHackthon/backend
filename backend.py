f54173bc0e003d4c4cf227eb661e1579a38f1867
Daniel Asfaw <asfawdaniel779@gmail.com>
2024-04-21 17:49:04 +0300

    add a code to send sms directly

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.rest import Client
+# from twilio.rest import Client
 
 app = FastAPI()
 
@@ -20,19 +20,19 @@ def sms_reply():
     # )
 
     # Your Twilio Account SID and Auth Token
-    account_sid = 'AC8934e412298d6b0e41de8c1789015651'
-    auth_token = '1083b10e0fe53c7e24385f10548742eb'
+    # account_sid = 'AC8934e412298d6b0e41de8c1789015651'
+    # auth_token = '1083b10e0fe53c7e24385f10548742eb'
 
-    # Initialize Twilio client
-    client = Client(account_sid, auth_token)
+    # # Initialize Twilio client
+    # client = Client(account_sid, auth_token)
 
-    # Send an SMS
-    message = client.messages.create(
-        body='Hello, this is a test message!',
-        from_='+12242316269',
-        to='+251984877774'
-    )
+    # # Send an SMS
+    # message = client.messages.create(
+    #     body='Hello, this is a test message!',
+    #     from_='+12242316269',
+    #     to='+251984877774'
+    # )
 
-    print(message.sid)
+    # print(message.sid)
 
     return str(resp)
