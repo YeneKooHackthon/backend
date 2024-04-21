e1b2abea2e5dbb0c0c5151a764c2a6069f0d2e8b
Daniel Asfaw <asfawdaniel779@gmail.com>
2024-04-21 17:44:10 +0300

    asdf

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
 
 app = FastAPI()
 
@@ -17,5 +18,21 @@ def sms_reply():
     # msg.media(
     #     "https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg"
     # )
+
+    # Your Twilio Account SID and Auth Token
+    account_sid = 'AC8934e412298d6b0e41de8c1789015651'
+    auth_token = '1083b10e0fe53c7e24385f10548742eb'
+
+    # Initialize Twilio client
+    client = Client(account_sid, auth_token)
+
+    # Send an SMS
+    message = client.messages.create(
+        body='Hello, this is a test message!',
+        from_='224231-6269',
+        to='0984877774'
+    )
+
+    print(message.sid)
 
     return str(resp)
