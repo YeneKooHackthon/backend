f9e999e25ff5ea012ca36d96b4024c062bde82d2
naolbm <profesornaoltena@gmail.com>
2024-04-25 14:37:30 +0300

    cors allowed

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, UploadFile, File
 from inference import inFerence
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict")
 async def predict(provider: str = None,  plant: str = None, file: UploadFile = File(...)):
