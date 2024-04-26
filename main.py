cf85aad8c8fd472e1c8a6a6de261ddabc32de032
naolbm <profesornaoltena@gmail.com>
2024-04-26 08:44:51 +0300

    unsplash api added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import requests
 from fastapi import FastAPI, UploadFile, File
 from inference import inFerence
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
 
 
 app = FastAPI()
+
+load_dotenv()
+
+UNSPLASH_KEY = os.getenv('UNSPLASH_KEY')
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,6 +19,25 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get('/plant/img')
+async def fetch_data(plant: str = "plant"):
+    url = f'https://api.unsplash.com/search/photos?client_id={UNSPLASH_KEY}&query=${plant}-plant&per_page=5'
+
+    url_array = []
+    response = requests.get(url)
+    data = response.json()
+    # print('data', data)
+
+    # Assuming data structure matches what is expected
+    for result in data['results']:
+        full_url = result['urls']['full']
+        url_array.append(full_url)
+
+    return url_array
+
+
+
 
 @app.post("/predict")
 async def predict(provider: str = None,  plant: str = None, file: UploadFile = File(...)):
